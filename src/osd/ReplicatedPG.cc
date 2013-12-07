014ded784ab848cdbcb05c39945d971a8832a446
Samuel Just <sam.just@inktank.com>
2013-12-07 15:10:26 -0800

    ReplicatedPG::finish_copyfrom: check new_obs.exists, not obs.exists
    If the transaction previously removed the object, the stat update
    will have already happened.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -4900,7 +4900,7 @@ int ReplicatedPG::finish_copyfrom(OpContext *ctx)
     ctx->op_t->remove(obs.oi.soid);
   }
 
-  if (!ctx->obs->exists) {
+  if (!obs.exists) {
     ctx->delta_stats.num_objects++;
     obs.exists = true;
   }
